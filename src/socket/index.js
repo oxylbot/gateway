a58a1af04df996cc7b12d243001cd9ddfb88d720
Owner <minemidnightmc@gmail.com>
2019-02-10 14:57:07 -0500

    Fix socket not exporting anything

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,13 +4,11 @@ const protobuf = require("protobufjs");
 const CacheSocket = require("./CacheSocket");
 const socket = new CacheSocket(process.env.CACHE_SOCKET_ADDRESS);
 
-async function init(database) {
+module.exports = async database => {
 	const cacheProto = await protobuf.load(path.resolve(__dirname, "..", "..", "protobuf", "Cache.proto"));
 
 	socket.start(cacheProto, database);
-}
-
-init();
+};
 
 process.on("SIGTERM", () => {
 	socket.close();
