5acbeab3cf606b175db365cde96cec3ea7ffa7df
minemidnight <minemidnightmc@gmail.com>
2019-06-08 00:50:51 -0400

    Add extra endpoints for users

--- src/rest/discord/users.js
+++ src/rest/discord/users.js
@@ -3,9 +3,23 @@ const router = module.exports = new (require("express").Router)();
 router.get("/", async (req, res) => {
 	const db = req.app.locals.db;
 
-	const userCount = await db.models.User.count();
+	if(req.query.name && req.query.discriminator) {
+		const users = await db.discord.users.getByNameAndDiscrim(req.params.name);
 
-	res.status(200).json({ users: userCount });
+		res.status(200).json(users);
+	} else if(req.query.discriminator) {
+		const users = await db.discord.users.getByDiscrim(req.params.discriminator);
+
+		res.status(200).json(users);
+	} else if(req.query.name) {
+		const users = await db.discord.users.getByName(req.params.name);
+
+		res.status(200).json(users);
+	} else {
+		const userCount = await db.models.User.count();
+
+		res.status(200).json({ userCount });
+	}
 });
 
 router.get("/:id", async (req, res) => {
