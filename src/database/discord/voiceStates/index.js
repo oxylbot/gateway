8c23aa107c07dfbc0843221896f2480923cd7f2b
minemidnight <minemidnightmc@gmail.com>
2019-07-21 15:58:07 -0400

    Fix voice state database queries

--- src/database/discord/voiceStates/index.js
+++ src/database/discord/voiceStates/index.js
@@ -1,4 +1,4 @@
-module.exports = async VoiceState => ({
+module.exports = VoiceState => ({
 	cache: require("./cache")(VoiceState),
 	getByGuildAndUserId: require("./getByGuildAndUser")(VoiceState),
 	getByChannelId: require("./getByChannelId")(VoiceState),
