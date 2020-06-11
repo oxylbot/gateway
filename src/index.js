5eb69ca7f598a07e643f85950b11f5684b18584a
minemidnight <minemidnightmc@gmail.com>
2020-06-10 21:14:01 -0400

    Remove extra socket code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const database = require("./database/index");
 const rest = require("./rest/index");
-const socket = require("./socket/index");
 
 async function init() {
 	const db = await database();
 
 	await rest(db);
-	await socket(db);
 }
 
 init();
