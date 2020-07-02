b4f44c1d0491ff36d192d6c4243157b9e1efbd29
minemidnight <minemidnightmc@gmail.com>
2020-07-02 19:17:43 -0400

    Fix syntax errors

--- src/rest/settings/autorolebot.js
+++ src/rest/settings/autorolebot.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router({ mergeParams: true }));
+const router = express.Router({ mergeParams: true });
 
 router.get("/", async (req, res) => {
 	const db = req.app.locals.db;
